179431bbb5b782a804209105a567057be5b3fe3a
ashish <ashishcg12@gmail.com>
2026-08-21 16:06:52 +0530

    feat: expose trajectory collection utilities

---
+++ src/data/__init__.py
@@ -0,0 +1,5 @@
+"""Data collection and dataset utilities."""
+
+from .collector import TrajectoryDataset, Transition, collect_trajectories
+
+__all__ = ["TrajectoryDataset", "Transition", "collect_trajectories"]
